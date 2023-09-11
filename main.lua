c83ef58252f7cfe3a000c788ff65be008bab6340
LinckA05 <143820870+LinckA05@users.noreply.github.com>
2023-09-11 14:30:46 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+print("Enter the length: ")
+length = io.read()
+print("Enter the width: ")
+width = io.read()
+
+area = length * width
+parameter = (length * 2) + (width * 2)
+
+print("The area is:",area)
+print("The parameter is:",parameter)
